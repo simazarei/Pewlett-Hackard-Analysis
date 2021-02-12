9331644596a39d726af24a7327340cc6f2b64825
sima zarei <simaa.zarei@gmail.com>
2021-02-12 01:39:22 -0500

    Adding quries.sql file to git.

--- schema.sql
+++ schema.sql
@@ -48,7 +48,7 @@ CREATE TABLE dept_emp (
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
-  title VARCHAR(4) NOT NULL,
+  title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
