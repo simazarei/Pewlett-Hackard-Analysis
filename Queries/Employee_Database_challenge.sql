3cdefe2e99273d0e868c51401c743cc6da46eff6
sima zarei <simaa.zarei@gmail.com>
2021-02-14 04:27:07 -0500

    Editing errors.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -23,6 +23,21 @@ title
 FROM retirement_titles AS rt
 ORDER BY emp_no ASC, 
 	to_date DESC;
+
+-- Modified querry without duplicate values
+SELECT DISTINCT ON (emp_no) e.emp_no,
+    e.first_name,
+e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+--INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	ORDER BY emp_no ASC,
+			to_date DESC;
 	
 -- Title count from unigue titles
 -- SELECT COUNT(emp_no) FROM unique_titles;
